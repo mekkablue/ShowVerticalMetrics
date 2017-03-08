20e4a6280f2fc735d888246e39b45148c209b37c
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2017-03-08 12:11:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# ShowVerticalMetrics.glyphsReporter
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert. After installation, it will add the menu item *View > Show Vertical Metrics*. You can set a keyboard shortcut in System Preferences.
+
+![Vertical Metrics](ShowVerticalMetrics.png "Show Vertical Metrics Screenshot")
+
+The plug-in will visualize the parameters `hheaAscender`, `hheaDescender`, `typoAscender`, `typoDescender`, `winAscent`, and `winDescent`, if present in *File > Font Info > Masters*.
+
+Thanks to @tntype for the idea.
+
+### Installation
+
+1. One-click install *Show Vertical Metrics* from *Window > Plugin Manager*
+2. Restart Glyphs.
+
+or: 
+
+1. Download the complete ZIP file and unpack it, or clone the repository.
+2. Double click the .glyphsReporter file. Confirm the dialog that appears in Glyphs.
+3. Restart Glyphs
+
+### Usage Instructions
+
+1. Open at least one glyph in Edit View.
+2. Use *View > Show Vertical Metrics* to toggle the display of the Vertical Metrics.
+
+### Requirements
+
+The plugin needs Glyphs 2.4 or higher, running on OS X 10.9 or later. It does NOT work with Glyphs 1.x.
+
+### License
+
+Copyright 2015-2017 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ ShowVerticalMetrics.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,69 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+
+from GlyphsApp.plugins import *
+
+class ShowVerticalMetrics(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({'en': u'Vertical Metrics', 'de': u'Vertikalmaße'})
+		self.verticalMetrics = (
+			"hheaAscender",
+			"hheaDescender",
+			# "hheaLineGap",
+			"typoAscender",
+			"typoDescender",
+			# "typoLineGap",
+			"winAscent",
+			"winDescent"
+		)
+		
+	def background(self, layer):
+		defaultColor = NSColor.greenColor()
+		defaultColor.set()
+		thisMaster = layer.associatedFontMaster()
+		heightsAlreadyUsed = []
+		xPosition = self.controller.viewPort.origin.x - self.controller.selectedLayerOrigin.x
+		if thisMaster:
+			for thisMetric in self.verticalMetrics:
+				height = thisMaster.customParameters[thisMetric]
+				if height:
+					if thisMetric == "winDescent":
+						height *= -1
+
+					alignment = "bottomright"
+					if height in heightsAlreadyUsed:
+						alignment = "topright"
+						if "win" in thisMetric:
+							alignment = "bottomleft"
+					else:
+						zoomFactor = self.getScale()
+						heightsAlreadyUsed.append(height)
+						line = NSBezierPath.bezierPath()
+						line.moveToPoint_( NSPoint(-50000, height) )
+						line.lineToPoint_( NSPoint(+50000, height) )
+						line.setLineWidth_( 1.0/zoomFactor )
+						line.setLineDash_count_phase_( [1.0/zoomFactor, 3.0/zoomFactor], 2, 3.5/zoomFactor )
+						line.stroke()
+					
+					self.drawTextAtPoint(
+						"  "+thisMetric+"  ", 
+						NSPoint(
+							(xPosition+80)/zoomFactor, 
+							height+2/zoomFactor if "bottom" in alignment else height,
+							),
+						fontColor=defaultColor,
+						align=alignment
+						)
+
